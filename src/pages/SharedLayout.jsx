c27488c80a5996de1683a9b6cba32115f096e8d3
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-04-16 21:03:01 -0300

    feat: add border nav

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,13 +1,16 @@
 import {Outlet, Link} from "react-router-dom";
+import {useLocation} from "react-router-dom";
 
 function Nav() {
+    const location = useLocation()
+
     return (
         <>
-        <nav className='pt-[1.5rem] pb-[.5rem] bg-white border-b-4'>
+        <nav className='pt-[1.5rem] pb-[.5rem] bg-white'>
             <ul className='flex justify-center gap-[3rem]'>
-                <li>Sacola</li>
-                <li>Pagamento</li>
-                <li>Confirmação</li>
+                <li className={location.pathname === "/" ? "border-b-4 border-black rounded-[.5rem]" : "border-b-4 rounded-[.5rem]"}>Sacola</li>
+                <li className={location.pathname === "/pay" ? "border-b-4 border-black rounded-[.5rem]" : "border-b-4 rounded-[.5rem]"}>Pagamento</li>
+                <li className={location.pathname === "/confirmPay" ? "border-b-4 border-black rounded-[.5rem]" : "border-b-4 rounded-[.5rem]"}>Confirmação</li>
             </ul>
         </nav>
             <Outlet/>
